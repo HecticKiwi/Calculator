9b26a953551c15a0e6f8f64a9b5513c594b06b4c
Danny <dannyduong4.00@outlook.com>
2022-06-27 19:18:15 -0600

    Operation buttons disabled when appropriate

--- main.js
+++ main.js
@@ -1,36 +1,57 @@
-let expression = "";
-expressionArray = [];
+let display = document.querySelector('.screen');
+let expression = [];
+let showingAnswer = false;
 
-const display = document.querySelector('.screen');
-display.innerHTML = expression;
-
-let expressionButtons = document.querySelectorAll('.number, .operation, .other');
-expressionButtons.forEach((button) => {
+let numbers = document.querySelectorAll('.number');
+numbers.forEach((button) => {
     button.addEventListener('click', () => {
-        expression += button.innerHTML;
-        display.innerHTML = expression;
-    })
+        if (showingAnswer) {
+            clear();
+        }
+        if (expression.length === 0 || isNaN(expression[expression.length - 1])) {
+            expression.push(button.innerHTML);
+        } else {
+            expression[expression.length - 1] += button.innerHTML;
+        }
+        console.log(expression);
+        display.innerHTML += button.innerHTML;
+        enableOperations();
+    });
+});
+
+let operations = document.querySelectorAll('.operation, .other');
+operations.forEach((button) => {
+    button.addEventListener('click', () => {
+        expression.push(button.innerHTML)
+        console.log(expression);
+        display.innerHTML += button.innerHTML;
+        disableOperations();
+    });
 });
 
 let AC = document.querySelector('.AC');
-AC.addEventListener('click', () => {
-    expression = '';
-    display.innerHTML = '';
-});
+AC.addEventListener('click', clear);
 
 let evaluate = document.querySelector('.evaluate');
-evaluate.addEventListener('click', () => calculate(expression));
-
-function operate(operation, a, b) {
-    switch (operation) {
-        case 'add': return add(a, b);
-        case 'subtract': return subtract(a, b);
-        case 'multiply': return multiply(a, b);
-        case 'divide': return divide(a, b);
+evaluate.addEventListener('click', () => {
+    let answer = calculate(expression);
+    if (answer.toString().length > 8) {
+        display.innerHTML = (+answer).toExponential(2);
+    } else {
+        display.innerHTML = answer;
     }
-}
+    showingAnswer = true;
+});
 
-calculate([3, "+", "(", "(", 4, "×", 3, ")", "+", "3", ")", "÷", "10"]);
+clear();
+// calculate([3, "+", "(", "(", 4, "×", 3, ")", "+", "3", ")", "÷", "10"]);
+
+function clear() {
+    expression = [];
+    display.innerHTML = '';
+    showingAnswer = false;
+    disableOperations();
+}
 
 function calculate(expression) {
     console.log("evaluating:");
@@ -40,7 +61,7 @@ function calculate(expression) {
     for (let i = expression.length; i >= 0; i--) {
         if (expression[i] === "(") {
             let j = expression.indexOf(")", i);
-            expression.splice(i, j - i + 1, calculate(expression.slice(i+1, j)));
+            expression.splice(i, j - i + 1, calculate(expression.slice(i + 1, j)));
             console.log(expression);
             console.log(i);
         }
@@ -48,22 +69,34 @@ function calculate(expression) {
 
     // Multiplication and division
     for (let i = 0; i < expression.length; i++) {
-        if (expression[i+1] === "×") {
-            expression.splice(i, 3, +expression[i] * +expression[i+2]);
-        } else if (expression[i+1] === "÷") {
-            expression.splice(i, 3, +expression[i] / +expression[i+2]);
+        if (expression[i + 1] === "×") {
+            expression.splice(i, 3, +expression[i] * +expression[i + 2]);
+        } else if (expression[i + 1] === "÷") {
+            expression.splice(i, 3, +expression[i] / +expression[i + 2]);
         }
     }
-    
+
     // Addition and subtraction
     for (let i = 0; i < expression.length; i++) {
-        if (expression[i+1] === "+") {
-            expression.splice(i, 3, +expression[i] + +expression[i+2]);
+        if (expression[i + 1] === "+") {
+            expression.splice(i, 3, +expression[i] + +expression[i + 2]);
         } else if (expression[i] === "-") {
-            expression.splice(i, 3, +expression[i] - +expression[i+2]);
+            expression.splice(i, 3, +expression[i] - +expression[i + 2]);
         }
     }
-    
+
     console.log(expression);
     return expression[0];
+}
+
+function enableOperations() {
+    operations.forEach((button) => {
+        button.disabled = false;
+    })
+}
+
+function disableOperations() {
+    operations.forEach((button) => {
+        button.disabled = true;
+    })
 }
